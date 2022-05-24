ca93ad9f51fab9335b7b8e8b6206331f53c017ce
Ezequiel Mensur <ezequiel.mensur@pampabi.com>
2022-05-24 17:43:38 +0000

    Correcion Slider

--- Rules/SetExtensionValue.js
+++ Rules/SetExtensionValue.js
@@ -1,7 +1,6 @@
 export default function SetExtensionValue(context) {
     console.log("In SetExtensionValue");
     let srcValue = context.getValue();
-    let targetCtrl = context.evaluateTargetPath("#Page:SalesOrderHeaders_Create/#Control:MyExtensionControlName");
-    console.log(targetCtrl)
+    let targetCtrl = context.evaluateTargetPath("#Page:SalesOrderHeaders_Create/#Control:GrossAmount");
     targetCtrl.setValue(srcValue);
 }
