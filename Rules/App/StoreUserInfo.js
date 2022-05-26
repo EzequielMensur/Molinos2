d9a18940848155ffc00e2720f2d3bfd5aba8d91a
Ezequiel Mensur <ezequiel.mensur@pampabi.com>
2022-05-26 17:40:37 +0000

    TestBottom3

--- Rules/App/StoreUserInfo.js
+++ Rules/App/StoreUserInfo.js
@@ -1,45 +1,12 @@
-/**
- * Almacena informacion relevante del usuario en las App Settings del dispositivo.
- */
 export default function StoreUserInfo(clientAPI) {
     let platform = clientAPI.nativescript.platformModule;
-    
-    if (platform.isIOS || platform.isAndroid) {
-        let appSettings = clientAPI.nativescript.appSettingsModule;
-        let appId = clientAPI.evaluateTargetPath('#Application/#ClientData/#Property:MobileServiceAppId');
-        let userInfoUrl = `/mobileservices/application/${appId}/roleservice/application/${appId}/v2/Me`;
-        let params = { 'method': 'GET' };
-        const userInfo = JSON.parse(r.content.toString());
-        if (appSettings.hasKey(`${appId}-UserName`)) {
-            // Si ya existe la informacion del usuario, no la recupera nuevamente.
-            return Promise.resolve();   
-        } else if (clientAPI.isDemoMode()) {
-            appSettings.setString(`${appId}-UserId`,'DemoUser');
-            appSettings.setString(`${appId}-UserName`,'DemoUser');
-            appSettings.setString(`${appId}-UserGivenName`,'Demo');
-            appSettings.setString(`${appId}-UserFamilyName`,'User');
-            appSettings.setString(`${appId}-UserEmail`,'mobileservicesclient@sap.com');
-            appSettings.setString(`${appId}-UserFullName`,`Demo User`);
-            return Promise.resolve();                 
-        } else {
+    let appSettings = clientAPI.nativescript.appSettingsModule;
+            let appId = clientAPI.evaluateTargetPath('#Application/#ClientData/#Property:MobileServiceAppId');
             let userInfoUrl = `/mobileservices/application/${appId}/roleservice/application/${appId}/v2/Me`;
             let params = { 'method': 'GET' };
-            clientAPI.sendRequest(userInfoUrl, params).then(r => {
-                if (r && r.statusCode === 200 && r.content) {
-                    const userInfo = JSON.parse(r.content.toString());
-                    alert(userInfo)
-                    appSettings.setString(`${appId}-UserId`,userInfo.id);
-                    appSettings.setString(`${appId}-UserName`,userInfo.userName);
-                    appSettings.setString(`${appId}-UserGivenName`,userInfo.name.givenName);
-                    appSettings.setString(`${appId}-UserFamilyName`,userInfo.name.familyName);
-                    appSettings.setString(`${appId}-UserEmail`,userInfo.emails[0].value);
-                    appSettings.setString(`${appId}-UserFullName`,`${userInfo.name.givenName} ${userInfo.name.familyName}`);
-                    return Promise.resolve();
-                } })
             return clientAPI.sendRequest(userInfoUrl, params).then(r => {
                 if (r && r.statusCode === 200 && r.content) {
                     const userInfo = JSON.parse(r.content.toString());
-                    alert(userInfo)
                     appSettings.setString(`${appId}-UserId`,userInfo.id);
                     appSettings.setString(`${appId}-UserName`,userInfo.userName);
                     appSettings.setString(`${appId}-UserGivenName`,userInfo.name.givenName);
@@ -52,8 +19,4 @@ export default function StoreUserInfo(clientAPI) {
             (error) => {
                 console.log(error.toString());
             });                
-        }
-    } else { 
-        return Promise.resolve();
-    }
 }
