13817a63d3993b0403f83ef2fa203658212a85b1
Ezequiel Mensur <ezequiel.mensur@pampabi.com>
2022-05-28 23:23:35 +0000

    EsconderPantalla

--- Rules/Hi.js
+++ Rules/Hi.js
@@ -2,15 +2,14 @@
  * Describe this function...
  * @param {IClientAPI} clientAPI
  */
-export default function Hi(clientAPI) {
+export default async function Hi(clientAPI) {
     let sideDrawerAPI = clientAPI.getGlobalSideDrawerControlProxy();
 
     let appId = clientAPI.evaluateTargetPath('#Application/#ClientData/#Property:MobileServiceAppId');
     let userInfoUrl = `/mobileservices/application/${appId}/roleservice/application/${appId}/v2/Me`;
     let params = { 'method': 'GET' };
-    let actions =sideDrawerAPI.actionResults
 
-    return clientAPI.sendRequest(userInfoUrl, params).then(r => {
+    return await clientAPI.sendRequest(userInfoUrl, params).then(r => {
         if (r && r.statusCode === 200 && r.content) {
             const userInfo = JSON.parse(r.content.toString());
             let userRoles = userInfo.roles;
